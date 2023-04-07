905c429c0ebe7e6eee702cb8fd2f3996ba0f12aa
SWARAJ PATIL <SWARAJ PATIL@LAPTOP-Q2KE6E5I>
2023-04-07 15:49:53 +0530

    DayOne Commit

--- Dayone/src/tns/Chardemo.java
+++ Dayone/src/tns/Chardemo.java
@@ -5,7 +5,7 @@ public class Chardemo {
 	public static void main(String[] args) {
 		char ch='a';
 		System.out.println(ch);
-		//asssign number to character
+		//assign number to character
 		char ch1=70;
 		System.out.println(ch1);
 		//assign unicode to character
