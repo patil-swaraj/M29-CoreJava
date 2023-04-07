60d53ffcc1526dc28b7c547c1cd4bcd8d2c3d527
SWARAJ PATIL <SWARAJ PATIL@LAPTOP-Q2KE6E5I>
2023-04-07 12:22:42 +0530

    First Commit

---
+++ Dayone/src/tns/PrimitiveDatatype.java
@@ -0,0 +1,20 @@
+package tns;
+
+public class PrimitiveDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bytemax=127;
+		short shortmax=32767;
+		long var1=5464364L;
+		char var2='y';
+		boolean flag=true;
+		System.out.println(bytemax);
+		System.out.println(shortmax);
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(flag);
+
+	}
+
+}
