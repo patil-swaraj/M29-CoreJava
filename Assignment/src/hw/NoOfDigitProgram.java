3ef6f2c0a5a00260625c1d2037c8589c6ba961d1
SWARAJ PATIL <SWARAJ PATIL@LAPTOP-Q2KE6E5I>
2023-04-17 13:40:10 +0530

    hw commit

---
+++ Assignment/src/hw/NoOfDigitProgram.java
@@ -0,0 +1,16 @@
+package hw;
+
+public class NoOfDigitProgram {
+
+	public static void main(String[] args) {
+		int num=12345465, count=0;
+		while(num != 0) {
+			num = num/10;
+			count++;
+			
+		}
+		System.out.println("Number of digit is: "+count);
+
+	}
+
+}
