e822998f4f240953c261c543e0c1b18d4a3c262c
SWARAJ PATIL <SWARAJ PATIL@LAPTOP-Q2KE6E5I>
2023-05-22 14:47:38 +0530

    Second Commit

---
+++ dayseven/src/thiskeyworddemo/ThisKeyword.java
@@ -0,0 +1,11 @@
+package thiskeyworddemo;
+
+public class ThisKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThisKeywordExample t =new ThisKeywordExample(555, 44365434L);
+		t.print();
+	}
+
+}
