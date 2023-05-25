a7f5e7b6ce1be00f7d90a21aa8f52c54b60afd13
SWARAJ PATIL <SWARAJ PATIL@LAPTOP-Q2KE6E5I>
2023-05-25 11:18:08 +0530

    First Commit

---
+++ Dayten/src/stringsprogram/StringBufferExample.java
@@ -0,0 +1,14 @@
+package stringsprogram;
+
+public class StringBufferExample {
+
+	public static void main(String[] args) {
+		
+		StringBuffer str1 = new StringBuffer("Niket");
+		System.out.println(str1.capacity());
+		System.out.println(str1.charAt(2));
+		
+
+	}
+
+}
---
+++ Dayten/src/stringsprogram/StringMethodProgram.java
@@ -0,0 +1,21 @@
+package stringsprogram;
+
+import java.util.*;
+
+public class StringMethodProgram {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Dhoni";
+		System.out.println("Enter the name");
+		Scanner s = new Scanner(System.in);
+		String str2 = new String(s.nextLine());
+		
+		if (str1.equals(str2))
+			System.out.println("Both are Equal");
+		else
+			System.out.println("Not Equal");
+
+	}
+
+}
